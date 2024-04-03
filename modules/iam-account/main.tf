2df75703e8880619b4e08e143e77e955f5dbf696
Yanara Velásquez <yanara.velasquez@thoughtworks.com>
2024-04-03 18:17:14 -0300

    test

--- modules/iam-account/main.tf
+++ modules/iam-account/main.tf
@@ -1,21 +1,17 @@
-data "aws_caller_identity" "this" {
-  count = var.get_caller_identity ? 1 : 0
-}
+# esto es una prueba
 
-resource "aws_iam_account_alias" "this" {
-  account_alias = var.account_alias
-}
-
-resource "aws_iam_account_password_policy" "this" {
-  count = var.create_account_password_policy ? 1 : 0
-
-  max_password_age               = var.max_password_age
-  minimum_password_length        = var.minimum_password_length
-  allow_users_to_change_password = var.allow_users_to_change_password
-  hard_expiry                    = var.hard_expiry
-  password_reuse_prevention      = var.password_reuse_prevention
-  require_lowercase_characters   = var.require_lowercase_characters
-  require_uppercase_characters   = var.require_uppercase_characters
-  require_numbers                = var.require_numbers
-  require_symbols                = var.require_symbols
+resource "aws_iam_role" "iam_role" {
+  name = "example_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
